3aea77d093bdedddf3d979cb08105d5011d01cd0
Tomasz Warkocki <warkocz@codequest.com>
2014-08-25 14:57:05 +0200

    Added specs for image_url method from  Asset class

--- spec/lib/contentful/management/asset_spec.rb
+++ spec/lib/contentful/management/asset_spec.rb
@@ -396,6 +396,18 @@ module Contentful
         end
       end
 
+      describe '#image_url' do
+        it 'empty_query' do
+          asset = Contentful::Management::Asset.new
+          asset.file = double('file', url: 'http://assets.com/asset.jpg')
+          expect(asset.image_url).to eq 'http://assets.com/asset.jpg'
+        end
+        it 'with_params' do
+          asset = Contentful::Management::Asset.new
+          asset.file = double('file', url: 'http://assets.com/asset.jpg')
+          expect(asset.image_url(w: 100, h: 100, fm: 'format', q: 1)).to eq 'http://assets.com/asset.jpg?w=100&h=100&fm=format&q=1'
+        end
+      end
     end
   end
 end
