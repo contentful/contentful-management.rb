7d0804eba67da565ddbc79bcebb9003e37e0b8c3
David Litvak <david.litvakb@gmail.com>
2017-10-04 12:09:06 -0300

    Add Organizations Endpoint

---
+++ lib/contentful/management/client_organization_methods_factory.rb
@@ -0,0 +1,23 @@
+require_relative 'client_association_methods_factory'
+
+module Contentful
+  module Management
+    # Wrapper for Organizations API for usage from within Client
+    # @private
+    class ClientOrganizationMethodsFactory
+      include Contentful::Management::ClientAssociationMethodsFactory
+
+      def new(*)
+        fail 'Not supported'
+      end
+
+      def find(*)
+        fail 'Not supported'
+      end
+
+      def all(*)
+        super(nil)
+      end
+    end
+  end
+end
---
+++ lib/contentful/management/organization.rb
@@ -0,0 +1,19 @@
+require_relative 'resource'
+
+module Contentful
+  module Management
+    # Resource class for Organization.
+    # @see _ https://www.contentful.com/developers/docs/references/content-management-api/#/reference/organizations
+    class Organization
+      include Contentful::Management::Resource
+      include Contentful::Management::Resource::SystemProperties
+      include Contentful::Management::Resource::Refresher
+
+      property :name
+
+      def self.build_endpoint(_endpoint_options)
+        '/organizations'
+      end
+    end
+  end
+end
