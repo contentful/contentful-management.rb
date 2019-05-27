01aea952f22a86a255a5c7fb62374f226f49d0ab
Tim Masliuchenko <insside@gmail.com>
2019-05-27 14:42:57 +0300

    Fix EnvironmentAware#environment_id when building entries or assets

--- lib/contentful/management/resource/environment_aware.rb
+++ lib/contentful/management/resource/environment_aware.rb
@@ -6,14 +6,17 @@ module Contentful
         # Gets the environment ID for the resource.
         def environment_id
           env = sys.fetch(:environment, {})
-          case env
-          when ::Hash
-            env.fetch(:id, 'master')
-          when ::Contentful::Management::Link
-            env.id
-          else
-            'master'
-          end
+          env_from_sys =
+            case env
+            when ::Hash
+              env.fetch(:id, nil)
+            when ::Contentful::Management::Link, ::Contentful::Management::Environment
+              env.id
+            end
+
+          return env_from_sys if env_from_sys
+
+          respond_to?(:content_type) && content_type && content_type.environment_id || 'master'
         end
       end
     end
