317c191a9e01875e2822ed9a8b16c3ae76df2d28
Bhushan Lodha <bhushanlodha@gmail.com>
2023-09-04 10:19:18 +0200

    transform hash valueS

--- lib/contentful/management/asset.rb
+++ lib/contentful/management/asset.rb
@@ -75,8 +75,8 @@ module Contentful
       # Parser for assets attributes, creates appropriate form of request.
       def fields_for_query
         self.class.fields_coercions.keys.each_with_object({}) do |field_name, results|
-          results[field_name] = @fields.each_with_object({}) do |(locale, fields), field_results|
-            field_results[locale] = get_value_from(fields, field_name)
+          results[field_name] = @fields.transform_values do |fields|
+            get_value_from(fields, field_name)
           end
         end
       end
